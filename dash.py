43e53e5162f7c742887e10a0fae2ee0014f5fab5
rayankbh <111236655+rayankbh@users.noreply.github.com>
2023-04-01 18:17:25 -0400

    Update

--- dash.py
+++ dash.py
@@ -1,5 +1,5 @@
-
 import dash
+import dash_auth
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
@@ -12,6 +12,7 @@ inventory_df = pd.read_csv("inventory.csv")
 
 # Create the app
 app = dash.Dash(__name__)
+server = app.server
 
 # Define the layout
 app.layout = html.Div(
@@ -45,39 +46,81 @@ app.layout = html.Div(
         ),
         html.Div(
             [
-                dcc.Graph(
-                    id="deliveries-graph",
-                    figure=px.bar(
-                        deliveries_df,
-                        x="Month",
-                        y="Deliveries",
-                        title="Monthly Deliveries",
-                    ),
+                html.H2("Data Table"),
+                html.Table(
+                    [
+                        html.Thead(
+                            html.Tr([html.Th(col) for col in deliveries_df.columns])
+                        ),
+                        html.Tbody(
+                            [
+                                html.Tr(
+                                    [html.Td(deliveries_df.iloc[i][col]) for col in deliveries_df.columns]
+                                )
+                                for i in range(len(deliveries_df))
+                            ]
+                        ),
+                    ]
                 ),
-                dcc.Graph(
-                    id="volunteers-graph",
-                    figure=px.line(
-                        volunteers_df,
-                        x="Month",
-                        y="Volunteers",
-                        title="Monthly Volunteers",
-                    ),
+            ],
+            className="data-container",
+        ),
+        html.Div(
+            [
+                dcc.Dropdown(
+                    id="filter-dropdown",
+                    options=[{"label": col, "value": col} for col in deliveries_df.columns],
+                    value="Month",
+                    clearable=False,
                 ),
-                dcc.Graph(
-                    id="inventory-graph",
-                    figure=px.area(
-                        inventory_df,
-                        x="Month",
-                        y="Inventory",
-                        title="Monthly Inventory",
-                    ),
-                ),
+                dcc.Graph(id="deliveries-graph"),
+                dcc.Graph(id="volunteers-graph"),
+                dcc.Graph(id="inventory-graph"),
             ],
             className="graphs-container",
         ),
     ],
     className="container",
 )
+
+# Add authentication
+VALID_USERNAME_PASSWORD_PAIRS = {
+    "admin": "password"
+}
+auth = dash_auth.BasicAuth(
+    app,
+    VALID_USERNAME_PASSWORD_PAIRS
+)
+
+# Define callbacks
+@app.callback(
+    [
+        dash.dependencies.Output("deliveries-graph", "figure"),
+        dash.dependencies.Output("volunteers-graph", "figure"),
+        dash.dependencies.Output("inventory-graph", "figure"),
+    ],
+    [dash.dependencies.Input("filter-dropdown", "value")],
+)
+def update_graphs(filter_col):
+    fig_deliveries = px.bar(
+        deliveries_df,
+        x=filter_col,
+        y="Deliveries",
+        title="Monthly Deliveries",
+    )
+    fig_volunteers = px.line(
+        volunteers_df,
+        x=filter_col,
+        y="Volunteers",
+        title="Monthly Volunteers",
+    )
+    fig_inventory = px.area(
+        inventory_df,
+        x=filter_col,
+        y="Inventory",
+        title="Monthly Inventory",
+    )
+    return fig_deliveries, fig_volunteers, fig_inventory
 
 # Run the app
 if __name__ == "__main__":
